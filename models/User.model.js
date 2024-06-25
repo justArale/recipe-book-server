dde8c8cd339dd36b5d3d2cd0ad68ae7de8212f79
justArale <just1arale@gmail.com>
2024-06-25 15:50:47 +0200

    set models

--- models/User.model.js
+++ models/User.model.js
@@ -1,8 +1,11 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
     email: {
       type: String,
       required: [true, "Email is required."],
@@ -14,10 +17,10 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
-    name: {
-      type: String,
-      required: [true, "Name is required."],
-    },
+    description: String,
+    image: String,
+
+    recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
