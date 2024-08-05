19a4d86ea5e74213252335afcc43eaa5e059ca6a
justArale <just1arale@gmail.com>
2024-08-05 15:26:03 +0200

    try to fix the bug for successfully deploying on vercel

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const express = require("express");
 
 const app = express();
 
-// Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
